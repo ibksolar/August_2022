049cba53ab149f973cd97e0156f81236919aa58e
Ibikunle <ibksolar@gmail.com>
2023-02-08 10:26:52 -0600

    Created back up to keep earlier thoughts for creating vec_layer
    Tried algo on Alaska data

---
+++ system_test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 01:38:37 2023
+
+@author: i368o351
+"""
+
+print('I was able to get here')
+resp = input('Insert your favorite number or text')
+print(f'This is your favorite value {resp}')
+print('First test successful')
